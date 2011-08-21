0820e70d34ffbdc005600bd9f177c25576e89db5
Derek McLean <derek.l.mclean@gmail.com>
2011-08-20 19:31:12 -0700

    Solved problem #20

--- cpp/20.cpp
+++ cpp/20.cpp
@@ -18,130 +18,132 @@
 
 using namespace std;
 
-string toString(long double n) {
-        stringstream conv_stream;
-        conv_stream << setprecision(10000);
-        conv_stream << n;
-        return conv_stream.str();
-}
-
-
-bool notZero(string number) {
-	char zero = '0';
-	int len = number.size();
-	for (int i = 0; i < len; i++) {
-		if (number[i] != zero) {
-			return true;
-		}
-	}
-	return false;
-}
-
-string padLeft(string num, size_t numZ) {
-	string pad (numZ, '0');
-	return pad + num;
-}
-
-int intAt(string s, int index) {
-	char c = s[index];
-	return atoi(&c);
-}
-
-string add(string a, string b) {
-	size_t lenA = a.size();
-	size_t lenB = b.size();
-	if (lenA < lenB) {
-		a = padLeft(a, lenB - lenA);
-	} else if (lenA > lenB) {
-		b = padLeft(b, lenA - lenB);
-	}
-	size_t numLength = a.size();
+class Number {
+		short *numArray;
+		int numDigits;
+		int capacity;
+			
+	public:
+		Number (int capacity);
+		Number (int capacity, long number);
+		Number (const Number &cSource);
+		string str();
+		int getCapacity ();
+		int getLength ();
+		short* asArray() { return numArray; }
+		short operator[] (int index);
+		Number operator + (Number other);
+		Number operator * (Number other);
 	
-	for (int i = numLength-1; i > -1; i--) {
-		int digA = intAt(a, i);
-		for (int j = i; j > -1; j--) {
-			int digB = intAt(b, j);
-			int sum = digA + digB;
-			digA = sum / 10;
-			b[j] = toString(sum % 10)[0];
-		}
-		if (digA > 0) {
-			string overStr = toString(digA);
-			b = overStr + b;
-			a = string (overStr.size(), '0') + a;
-		}
-	}
-	return b;
+	private:
+		void init(int capacity);
+		void addToDigit(int index, short digit);
+		static Number multiply(Number a, Number b);
+};
+Number::Number (int capacity) {
+	init(capacity);
 }
-
-string mult(string a, string b) {
-	string product = "0";
-	int lenA = a.size();
-	int lenB = b.size();
-	for (int i = lenA - 1; i > -1; i--) {
-		int digA = intAt(a, i);
-		for (int j = lenB - 1; j > -1; j--) {
-			int digB = intAt(b, j);
-			int iProd = digA * digB;
-			string prod = toString(iProd) + string (lenA - i + lenB - j - 2, '0');
-			product = add(product, prod);
+Number::Number (int capacity, long number) {
+	init(capacity);	
+	while (number != 0) {
+		numArray[numDigits] = number % 10;
+		number /= 10;
+		numDigits++;
+	}
+}
+Number::Number (const Number &source) {
+	init(source.capacity);	
+	numDigits = source.numDigits;
+	for (int i = 0; i < numDigits; i++) {
+		numArray[i] = source.numArray[i];
+	}
+}
+void Number::init (int cap) {
+	capacity = cap;
+	numArray = new short[capacity];
+	for (int i = 0; i < capacity; i++) {
+		numArray[i] = 0;
+	}
+	numDigits = 0;
+}
+string Number::str () {
+	if (numDigits == 0) {
+		return "NULL";
+	}
+	stringstream conv_stream;
+    conv_stream << setprecision(numDigits);
+	for (int i = numDigits-1; i > -1; i--) {
+		conv_stream << numArray[i];
+	}
+	return conv_stream.str();
+}
+void Number::addToDigit(int index, short digitValue) {
+	if (digitValue !=0) {
+		short sum = numArray[index] + digitValue;
+		numArray[index] = sum % 10;
+		addToDigit(index+1, sum/10);
+		if (index >= numDigits) {
+			numDigits = index + 1;
 		}
 	}
-	cout << product << endl;
-	return product;	
+}
+int Number::getCapacity () {
+	return capacity;
+}
+int Number::getLength () {
+	return numDigits;
+}
+Number Number::operator+ (Number other) {
+	Number sum (*this);
+	for (int i = 0 ; i < sum.getLength(); i++) {
+		sum.addToDigit(i, other[i]);
+	}
+	return (sum);
+}
+short Number::operator[] (int index) {
+	return numArray[index];
+}
+Number Number::operator* (Number other) {
+	int otherCap = other.getCapacity();
+	if (otherCap > capacity) {
+		return multiply(other, *this);
+	} else {
+		return multiply(*this, other);
+	}
+}
+/**
+ * REQUIRED:
+ * a >= b
+ */
+Number Number::multiply(Number a, Number b) {
+	Number product (a.getCapacity());
+	short dA, dB, prod;
+	for (int bIndex = 0 ; bIndex < b.getLength(); bIndex++) {
+		dB = b[bIndex];
+		for (int aIndex = 0; aIndex < a.getLength(); aIndex++) {
+			dA = a[aIndex];
+			prod = dA * dB;
+			product.addToDigit(aIndex+bIndex,prod);
+		}
+	}
+	return product;
 }
 
 int main(int argc, char* argv[]) {
-	int base = atoi(argv[1]);
-	cout << "Base: " << base << endl;
-
-	string fact = "1";	
-	for (int i = 1; i <= base; i++) {
-		string n = toString(i);
-		cout << n << ", ";
-		fact = mult(fact, n);
-		cout << fact << endl;
+	int a = atoi(argv[1]);
+	Number base (500, a);
+	cout << "BASE: " << base.str() << endl;
+	
+	Number fact (500, 1);
+	for (int i = a; i > 0; i--) {
+		Number n (500, i);
+		fact = fact * n;
 	}
-	
-	cout << "Factorial: " << fact << endl;
-	
-	return 0;
-}
-/*
-long factorial(int base) {
-	long long num = 1;
-	for (int i = base; i > 0; i--) {
-		cout << i << " - ";
-		num *= i;
-		cout << num << endl;
-	}
-	return num;
-}
-
-string toString(long double n) {
-        stringstream conv_stream;
-	conv_stream << setprecision(10000);
-        conv_stream << n;
-        return conv_stream.str();
-}
-
-int main(int argc, char* argv[]) {
-	cout << setprecision(1000);	
-	int base = atoi(argv[1]);
-	cout << "Start: " << base << endl;
-	
-	long fact = factorial(base);
-	string digits = toString(fact);
-	int len = digits.size();
 	int sum = 0;
-	for (int i = 0; i < len; i++) {
-		char c = digits[i];
-		int digit = atoi(&c);
-		sum += digit;
+	for (int i = 0; i < fact.getLength(); i++) {
+		sum += fact[i];
 	}
-	
-	cout << endl << sum << endl;
+	cout << "Sum: " << sum << endl;
 	
 	return 0;
 }
-*/
